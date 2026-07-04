b58bc9adc0c2cd5b79e579a2cf07255652779c2e
Jio Park <nationaljiopark@gmail.com>
2026-07-04 22:19:19 +0100

    Add files via upload

---
+++ Blackjack/index.js
@@ -0,0 +1,7 @@
+// 1. Create two variables, firstCard and secondCard.
+let firstCard = 3;
+let secondCard = 9;
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
