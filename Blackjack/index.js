be3fd0985892a81f4c8337eeb765d4d53870b5e7
Jio Park <nationaljiopark@gmail.com>
2026-07-04 23:42:18 +0100

    Update index.js

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,7 +1,17 @@
-// 1. Create two variables, firstCard and secondCard.
-let firstCard = 3;
-let secondCard = 9;
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let firstCard = 10;
+let secondCard = 7;
 let sum = firstCard + secondCard;
+
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+
+if (sum <= 20) {
+  console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+  console.log("You're out of the game! 😭");
+}
