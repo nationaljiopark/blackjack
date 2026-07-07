293d252f9545c3cac91ee8f3957f7f42034ef096
Jio Park <nationaljiopark@gmail.com>
2026-07-07 21:13:24 +0100

    Update index.js

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,14 +1,23 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards=[firstCard,secondCard]// array - ordered list of items
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-// 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el");
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el");
+// Create a new function called startGame() that calls renderGame()
 function startGame() {
-  // 3. Render the sum on the page using this format -> "Sum: 14"
+  renderGame();
+}
+
+function renderGame() {
+  // render out firstCard and secondCard
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+// render out ALL the cards we have
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -20,4 +29,14 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  // 1. Create a card variable, and hard code its value to a number (2-11)
+  let card = 7;
+  // 2. Add the new card to the sum variable
+  sum += card;
+  // 3. Call startGame()
+  startGame();
 }
