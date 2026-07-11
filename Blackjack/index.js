5308d8c304a3384f2ab97ba6b75584843db475ca
Jio Park <nationaljiopark@gmail.com>
2026-07-11 12:07:31 +0100

    Update index.js

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,19 +1,25 @@
-let cards = []; // array - ordered list of items
+let player = {
+  name: "Jio",
+  chips: 200,
+};
+let cards = [];
 let sum = 0;
 let hasBlackJack = false;
-let isAlive = fulse;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-console.log(cards);
-
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playerEl = document.getElementById("player-el");
+// 4. Render the player's name and chips in playerEl
+playerEl.textContent = player.name + ":$" + player.chips;
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
   if (randomNumber > 10) {
     return 10;
   } else if (randomNumber === 1) {
-    return11;
+    return 11;
   } else {
     return randomNumber;
   }
@@ -21,24 +27,19 @@ function getRandomCard() {
 
 function startGame() {
   isAlive = true;
-  // Generate two random numbes
   let firstCard = getRandomCard();
   let secondCard = getRandomCard();
   cards = [firstCard, secondCard];
   sum = firstCard + secondCard;
-  // Re-assign the cards and sum variables so that the game can start
   renderGame();
 }
 
 function renderGame() {
-  // 2. Refer to the cards array when rendering out the cards
   cardsEl.textContent = "Cards: ";
-  // Create a for loop that renders out all the cards instead of just two
   for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " ";
   }
 
-  // render out ALL the cards we have
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -51,12 +52,12 @@ function renderGame() {
   }
   messageEl.textContent = message;
 }
-// 3. Use the getRandomCard() to set the value of card
+
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  // Push the card to the cards array
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
